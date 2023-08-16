842f20607138e2c933bc77f8ae1e7bb3914a364f
scelsic2 <scelsic2@gmail.com>
2023-08-16 13:16:48 -0400

    index example

---
+++ assets/js/index_EXAMPLE.js
@@ -0,0 +1,6 @@
+const apiURL = `https://api.petfinder.com/v2/animals?type=`
+const header = `"Authorization" : "Bearer MYTOKEN"`
+
+const acOrigin = `"access-control-allow-origin": "*"`
+const acHeaders = `"Access-Control-Allow-Headers": "Content-Type, Authorization"`
+const acMethods = `"Access-Control-Allow-Methods": "*"`
